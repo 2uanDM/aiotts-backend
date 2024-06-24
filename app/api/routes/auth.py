7ebc34cc78d56bea63b1049d6e29f89a6dc22c98
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2024-06-24 13:18:03 +0700

    feat: adding some auth apis

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+import os
+
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
@@ -7,6 +10,9 @@ from app.database.crud import get_password_status, get_uuid
 from app.database.init import SessionLocal
 
 router = APIRouter()
+
+
+load_dotenv(override=True)
 
 
 # Dependency
@@ -33,7 +39,7 @@ def check_uuid(value: str, db: Session = Depends(get_db)):
     )
 
 
-@router.get("/search/password_status", tags=["Authentication"])
+@router.get("/search/password-status", tags=["Authentication"])
 def check_password_status(email: str, db: Session = Depends(get_db)):
     personnel: models.Personnel = get_password_status(email, db)
     if personnel is None:
@@ -51,4 +57,16 @@ def check_password_status(email: str, db: Session = Depends(get_db)):
         return JSONResponse(
             status_code=200,
             content={"status": "changed"},
+        )
+
+
+@router.get("/search/login-expired-day", tags=["Authentication"])
+def check_login_expired_day():
+    expired_days: str = os.getenv("LOGIN_EXPIRED_DAYS")
+
+    if not expired_days:  # Default value is 1 day
+        return Response(content="1", status_code=200, media_type="application/text")
+    else:
+        return Response(
+            content=expired_days, status_code=200, media_type="application/text"
         )
