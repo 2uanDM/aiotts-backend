49c8fb94d17e9a84723e8bd95083863d9a6cc449
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2024-08-22 14:25:19 +0700

    feat: Add SKUSToInsert model for inserting SKU data

--- app/api/schema/google_sheet.py
+++ app/api/schema/google_sheet.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel, Field
 
 
-class SKU(BaseModel):
-    sku_id: str = Field(..., title="SKU ID to search", example="1729491015689212227")
+class SKUSToInsert(BaseModel):
+    sku_id: str = Field(..., title="SKU ID to insert", example="1729464933078897337")
+    color: str = Field(..., title="Color of SKU", example="Black")
+    product_type: str = Field(..., title="Type of product", example="T-Shirt")
+    size: str = Field(..., title="Size of product", example="XL")
+    seller_sku: str = Field(..., title="Seller SKU", example="SKU-123")
+    product_name: str = Field(..., title="Name of product", example="T-Shirt Black XL")
 
 
-__all__ = ["SKU"]
+__all__ = ["SKUSToInsert"]
